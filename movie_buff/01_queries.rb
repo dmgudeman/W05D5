c5421bcee8f6f4b796040ffb19a66d15cc85d94d
David M Gudeman <davidmgudeman@gmail.com>
2023-03-03 14:08:11 -0800

    finish 2nd example

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -7,6 +7,8 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
 
+  Movie.select( :id, :title, :score ).where(score: 2..3)
+
 end
 
 def harrison_ford
@@ -20,6 +22,12 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
+  Movie
+     .joins(:actors)
+     .select( :id, :title)
+     .where(actors: { name: 'Harrison Ford'})
+     .where.not(castings: {ord: 1})
+
 
 end
 
